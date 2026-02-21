43f8ccb6a50eefa2b66eaba02ac5ba1239f38edd
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-02-21 10:52:15 -0600

    feat: added opening connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 pub mod tests;
+use virt::connect::Connect;
 fn main() {
     println!("Hello, world!");
+    if let Ok(mut conn) = Connect::open(Some("test:///default")) {
+        assert_eq!(Ok(0), conn.close());
+    }
 }
